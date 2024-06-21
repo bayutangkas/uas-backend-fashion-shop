a7047170aefefad2f3dd55eae3c1736c7514fa06
bayutangkas <bayutngks@gmail.com>
2024-06-21 23:10:17 +0800

    Implemented readAllOrders method to fetch all orders from the database and handled possible PDO exceptions

--- models/OrderModel.php
+++ models/OrderModel.php
@@ -9,4 +9,19 @@ class OrderModel
         $this->conn = $db;
         $this->table_name = 'orders';
     }
+    
+    public function readAllOrders()
+    {
+        try
+        {
+            $query = "SELECT * FROM " . $this->table_name;
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+            return $stmt;
+        }
+        catch (PDOException $e)
+        {
+            echo "Error: " . $e->getMessage();
+        }
+    }
 }
