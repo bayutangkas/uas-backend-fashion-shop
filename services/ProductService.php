af2ac7d23e5e864a5cd12cac8d429a55dc92414f
bayutangkas <bayutngks@gmail.com>
2024-06-22 23:11:32 +0800

    Develop fetchAllProducts functionality in ProductService

--- services/ProductService.php
+++ services/ProductService.php
@@ -9,4 +9,27 @@ class ProductService
     {
         $this->productModel = $productModel;
     }
+
+    public function fetchAllProducts()
+    {
+        $products = $this->productModel->readAllProducts();
+        $products_array = array();
+        $products_array["records"] = array();
+        while ($rows = $products->fetch(PDO::FETCH_ASSOC))
+        {
+            extract($rows);
+            $product_item = array(
+                "id_product" => $id_product,
+                "id_category" => $id_category,
+                "product_name" => $product_name,
+                "photo" => $photo,
+                "description" => $description,
+                "stock" => $stock,
+                "price" => $price
+            );
+            array_push($products_array["records"], $product_item);
+        }
+        
+        return $products_array;
+    }
 }
