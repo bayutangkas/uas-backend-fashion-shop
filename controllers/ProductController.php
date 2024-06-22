28e639905b8afbb800e82b43ae9c86a4df8ea55a
bayutangkas <bayutngks@gmail.com>
2024-06-22 23:27:18 +0800

    Create ProductController with dependency setup

---
+++ controllers/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once 'models/ProductModel.php';
+include_once 'services/ProductService.php';
+
+class ProductController
+{
+    private $productService;
+
+    public function __construct($conn)
+    {
+        $productModel = new ProductModel($conn);
+        $this->productService = new ProductService($productModel);
+    }
+}
